83db12cb5907a91c5525f4ae5690344a7071cf0e
Glenn Deschuymer <glenn.deschuymer@gmail.com>
2022-08-14 17:20:48 +0200

    Diffview

--- .config/nvim/lua/gdeschuy/plugins/neogit.lua
+++ .config/nvim/lua/gdeschuy/plugins/neogit.lua
@@ -1,8 +1,13 @@
 local function init()
-    require("neogit").setup{
+    local ok, neogit = pcall(require, "neogit")
+    if not ok then
+        return
+    end
+    
+    neogit.setup {
         integrations = {
-            diffview = true
-        }
+            diffview = true,
+        },
     }
 end
 
--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 CONFIG_DIR="/home/gdeschuy/.config"
 
-echo "### Updating package manager ###"
+add-apt-repository -y ppa:git-core/ppa
 add-apt-repository -y ppa:neovim-ppa/unstable
+
 apt-get update
 
-echo "### Installing packages ###"
 apt-get install -y fzf \
 	git \
 	neovim
