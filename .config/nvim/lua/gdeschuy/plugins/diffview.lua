6c6223dd3e5f0b53676dfa4b5181cce9e73ba5cf
Glenn Deschuymer <glenn.deschuymer@gmail.com>
2022-08-15 17:39:28 +0200

    Diffview

--- .config/nvim/lua/gdeschuy/plugins/diffview.lua
+++ .config/nvim/lua/gdeschuy/plugins/diffview.lua
@@ -1,8 +1,49 @@
 local function init()
     require("diffview").setup({
         diff_binaries = false,
+        enhanced_diff_hl = true,
         git_cmd = { "git" },
-        use_icons = false
+        use_icons = true,
+         icons = {
+             folder_closed = "",
+             folder_open = "",
+         },
+         signs = {
+             fold_closed = "",
+             fold_open = "",
+         },
+         file_panel = {
+            listing_style = "tree",
+            tree_options = {
+                flatten_dirs = true,
+                folder_statuses = "only_folded",
+            },
+            win_config = {
+                position = "left",
+                width = 35,
+            },
+            file_history_panel = {
+                log_options = {
+                    single_file = {
+                        diff_merges = "combined",
+                    },
+                    multi_file = {
+                        diff_merges = "first-parent",
+                    },
+                },
+                win_config = {
+                    position = "bottom",
+                    height = 16,
+                },
+            },
+            commit_log_panel = {
+                win_config = {},
+            },
+            default_args = {
+                DiffviewOpen = {},
+                DiffviewFileHistory = {},
+            }
+        }
     })
 end
 
